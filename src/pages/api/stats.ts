54e224f5e971dec29ee5d0f46e5907a784084e46
Marcelo Adamatti <adamatti@gmail.com>
2022-03-11 11:23:37 -0300

    feat(stats): secure endpoint

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -1,7 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from "next-auth/jwt"
 import {getStats} from "../../server/statsService";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const token = await getToken({ req })
+  if (!token) {
+    res.status(401).end()
+    return
+  }
+
   const stats = await getStats();
   res.status(200).json(stats);
 }
